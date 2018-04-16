d6998707d58d165dd7178b125b57923a5e46105b
Sydnie T <sydnietom@gmail.com>
2018-04-16 06:20:37 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('div div div div div');
+}
+
+function increaseRankBy(n) {
+  
+  var lis = document.querySelectorAll('ul.ranked-list li'); 
+
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div div div div div');
+}
+
